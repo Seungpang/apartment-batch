11948fb98d3caf3a13c1ee932a12437b835af174
Kim Seunglae <obey1342@gmail.com>
2021-10-06 15:17:14 +0900

    project init

---
+++ src/main/java/me/seungpang/apartmentbatch/ApartmentBatchApplication.java
@@ -0,0 +1,13 @@
+package me.seungpang.apartmentbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/seungpang/apartmentbatch/ApartmentBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seungpang.apartmentbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
