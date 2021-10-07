436cdc22260c10e1ea5eb5e3553aa8f4dfac2f6d
Kim Seunglae <obey1342@gmail.com>
2021-10-07 16:14:37 +0900

    Add JpaConfig

---
+++ src/main/java/me/seungpang/apartmentbatch/config/ApartmentBatchJpaConfig.java
@@ -0,0 +1,10 @@
+package me.seungpang.apartmentbatch.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class ApartmentBatchJpaConfig {
+
+}
