ed1ae71fb8b3db533b2e6305f121729367a0a3ec
Kim Seunglae <obey1342@gmail.com>
2021-10-12 01:46:56 +0900

    Conditional Flow Step 구현 및 확인

--- src/main/java/me/seungpang/apartmentbatch/job/apt/AptDealInsertJobConfig.java
+++ src/main/java/me/seungpang/apartmentbatch/job/apt/AptDealInsertJobConfig.java
@@ -11,6 +11,7 @@ import me.seungpang.apartmentbatch.core.repository.LawdRepository;
 import me.seungpang.apartmentbatch.job.validator.FilePathParameterVaildator;
 import me.seungpang.apartmentbatch.job.validator.LawdCdParameterValidator;
 import me.seungpang.apartmentbatch.job.validator.YearMonthParameterValidator;
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
@@ -49,15 +50,17 @@ public class AptDealInsertJobConfig {
     @Bean
     public Job aptDealInsertJob(
         Step guLawdCdStep,
-        Step contextPrintStep,
-        Step aptDealInsertStep
+        Step contextPrintStep
+//        Step aptDealInsertStep
     ) {
         return jobBuilderFactory.get("aptDealInsertJob")
             .incrementer(new RunIdIncrementer())
             .validator(aptDealJobParameterValidator())
             .start(guLawdCdStep)
-            .next(contextPrintStep)
-            .next(aptDealInsertStep)
+            .on("CONTINUABLE").to(contextPrintStep).next(guLawdCdStep)
+            .from(guLawdCdStep)
+            .on("*").end()
+            .end()
             .build();
     }
 
@@ -77,6 +80,13 @@ public class AptDealInsertJobConfig {
             .build();
     }
 
+    /**
+     * ExecutionContext에 저장할 데이터
+     * 1. guLawdCd - 구 코드 -> 다음 스텝에서 활용될 값
+     * 2. guLawdList - 구 코드 리스트
+     * 3. itemCount - 남아있는 구 코드 갯수
+     * @return
+     */
     @StepScope
     @Bean
     public Tasklet guLawdCdTasklet() {
@@ -85,8 +95,32 @@ public class AptDealInsertJobConfig {
             ExecutionContext executionContext = stepExecution.getJobExecution()
                 .getExecutionContext();
 
-            List<String> guLawdCd = lawdRepository.findDistinctGuLawdCd();
-            executionContext.putString("guLawdCd", guLawdCd.get(0));
+            // 데이터가 있으면 다음 스텝을 실행하도록 하고, 데이터가 없으면 종료되도록 한다.
+            // 데이터가 있으면 -> CONTINUABLE
+            List<String> guLawdCdList;
+            if (!executionContext.containsKey("guLawdCdList")) {
+                guLawdCdList = lawdRepository.findDistinctGuLawdCd();
+                executionContext.put("guLawdCdList", guLawdCdList);
+                executionContext.putInt("itemCount",guLawdCdList.size());
+            } else {
+                guLawdCdList = (List<String>)executionContext.get("guLawdCdList");
+            }
+
+            Integer itemCount = executionContext.getInt("itemCount");
+
+            if (itemCount == 0) {
+                contribution.setExitStatus(ExitStatus.COMPLETED);
+                return RepeatStatus.FINISHED;
+            }
+
+            itemCount--;
+
+            String guLawdCd = guLawdCdList.get(itemCount);
+
+            executionContext.putString("guLawdCd", guLawdCd);
+            executionContext.putInt("itemCount",itemCount);
+
+            contribution.setExitStatus(new ExitStatus("CONTINUABLE"));
 
             return RepeatStatus.FINISHED;
         };
