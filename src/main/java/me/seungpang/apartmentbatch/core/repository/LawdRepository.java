07ce143681c5d3062ace36eaf4f4b440ac5863fc
Kim Seunglae <obey1342@gmail.com>
2021-10-11 18:39:13 +0900

    구 코드를 읽는 Step 확인

--- src/main/java/me/seungpang/apartmentbatch/core/repository/LawdRepository.java
+++ src/main/java/me/seungpang/apartmentbatch/core/repository/LawdRepository.java
@@ -1,10 +1,15 @@
 package me.seungpang.apartmentbatch.core.repository;
 
+import java.util.List;
 import java.util.Optional;
 import me.seungpang.apartmentbatch.core.entity.Lawd;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LawdRepository extends JpaRepository<Lawd,Long> {
 
     Optional<Lawd> findByLawdCd(String lawdCd);
+
+    @Query("select distinct substring(l.lawdCd, 1, 5) from Lawd l where l.exist = 1 and l.lawdCd not like '%00000000'")
+    List<String> findDistinctGuLawdCd();
 }
--- src/main/java/me/seungpang/apartmentbatch/job/apt/AptDealInsertJobConfig.java
+++ src/main/java/me/seungpang/apartmentbatch/job/apt/AptDealInsertJobConfig.java
@@ -6,21 +6,26 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.seungpang.apartmentbatch.adapter.ApartmentApiResource;
 import me.seungpang.apartmentbatch.core.dto.AptDealDto;
+import me.seungpang.apartmentbatch.core.repository.LawdRepository;
 import me.seungpang.apartmentbatch.job.validator.FilePathParameterVaildator;
 import me.seungpang.apartmentbatch.job.validator.LawdCdParameterValidator;
 import me.seungpang.apartmentbatch.job.validator.YearMonthParameterValidator;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,13 +41,17 @@ public class AptDealInsertJobConfig {
     private final StepBuilderFactory stepBuilderFactory;
 
     private final ApartmentApiResource apartmentApiResource;
+    private final LawdRepository lawdRepository;
 
     @Bean
-    public Job aptDealInsertJob(Step aptDealInsertStep) {
+    public Job aptDealInsertJob(
+        Step guLawdCdStep
+        //Step aptDealInsertStep
+    ) {
         return jobBuilderFactory.get("aptDealInsertJob")
             .incrementer(new RunIdIncrementer())
-            .validator(aptDealJobParameterValidator())
-            .start(aptDealInsertStep)
+            //.validator(aptDealJobParameterValidator())
+            .start(guLawdCdStep)
             .build();
     }
 
@@ -54,6 +63,25 @@ public class AptDealInsertJobConfig {
         ));
         return validator;
     }
+
+    @JobScope
+    @Bean
+    public Step guLawdCdStep(Tasklet guLawdCdTasklet) {
+        return stepBuilderFactory.get("guLawdCdStep")
+            .tasklet(guLawdCdTasklet)
+            .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet guLawdCdTasklet() {
+        return (contribution, chunkContext) -> {
+            lawdRepository.findDistinctGuLawdCd()
+                .forEach(System.out::println);
+            return RepeatStatus.FINISHED;
+        };
+    }
+
 
     @JobScope
     @Bean
