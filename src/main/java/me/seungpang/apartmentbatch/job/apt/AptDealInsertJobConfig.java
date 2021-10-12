f5a70841966884e1b182597d6414d95cc041a10d
Kim Seunglae <obey1342@gmail.com>
2021-10-12 13:39:29 +0900

    Tasklet 클래스로 리팩토링

--- src/main/java/me/seungpang/apartmentbatch/job/apt/AptDealInsertJobConfig.java
+++ src/main/java/me/seungpang/apartmentbatch/job/apt/AptDealInsertJobConfig.java
@@ -2,30 +2,22 @@ package me.seungpang.apartmentbatch.job.apt;
 
 import java.time.YearMonth;
 import java.util.Arrays;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.seungpang.apartmentbatch.adapter.ApartmentApiResource;
 import me.seungpang.apartmentbatch.core.dto.AptDealDto;
 import me.seungpang.apartmentbatch.core.repository.LawdRepository;
-import me.seungpang.apartmentbatch.job.validator.FilePathParameterVaildator;
-import me.seungpang.apartmentbatch.job.validator.LawdCdParameterValidator;
 import me.seungpang.apartmentbatch.job.validator.YearMonthParameterValidator;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
@@ -33,7 +25,6 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 @Configuration
@@ -45,7 +36,6 @@ public class AptDealInsertJobConfig {
     private final StepBuilderFactory stepBuilderFactory;
 
     private final ApartmentApiResource apartmentApiResource;
-    private final LawdRepository lawdRepository;
 
     @Bean
     public Job aptDealInsertJob(
@@ -55,7 +45,7 @@ public class AptDealInsertJobConfig {
     ) {
         return jobBuilderFactory.get("aptDealInsertJob")
             .incrementer(new RunIdIncrementer())
-            .validator(aptDealJobParameterValidator())
+            .validator(new YearMonthParameterValidator())
             .start(guLawdCdStep)
             .on("CONTINUABLE").to(contextPrintStep).next(guLawdCdStep)
             .from(guLawdCdStep)
@@ -64,13 +54,13 @@ public class AptDealInsertJobConfig {
             .build();
     }
 
-    private JobParametersValidator aptDealJobParameterValidator() {
-        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
-        validator.setValidators(Arrays.asList(
-            new YearMonthParameterValidator()
-        ));
-        return validator;
-    }
+//    private JobParametersValidator aptDealJobParameterValidator() {
+//        CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+//        validator.setValidators(Arrays.asList(
+//            new YearMonthParameterValidator()
+//        ));
+//        return validator;
+//    }
 
     @JobScope
     @Bean
@@ -89,41 +79,8 @@ public class AptDealInsertJobConfig {
      */
     @StepScope
     @Bean
-    public Tasklet guLawdCdTasklet() {
-        return (contribution, chunkContext) -> {
-            StepExecution stepExecution = chunkContext.getStepContext().getStepExecution();
-            ExecutionContext executionContext = stepExecution.getJobExecution()
-                .getExecutionContext();
-
-            // 데이터가 있으면 다음 스텝을 실행하도록 하고, 데이터가 없으면 종료되도록 한다.
-            // 데이터가 있으면 -> CONTINUABLE
-            List<String> guLawdCdList;
-            if (!executionContext.containsKey("guLawdCdList")) {
-                guLawdCdList = lawdRepository.findDistinctGuLawdCd();
-                executionContext.put("guLawdCdList", guLawdCdList);
-                executionContext.putInt("itemCount",guLawdCdList.size());
-            } else {
-                guLawdCdList = (List<String>)executionContext.get("guLawdCdList");
-            }
-
-            Integer itemCount = executionContext.getInt("itemCount");
-
-            if (itemCount == 0) {
-                contribution.setExitStatus(ExitStatus.COMPLETED);
-                return RepeatStatus.FINISHED;
-            }
-
-            itemCount--;
-
-            String guLawdCd = guLawdCdList.get(itemCount);
-
-            executionContext.putString("guLawdCd", guLawdCd);
-            executionContext.putInt("itemCount",itemCount);
-
-            contribution.setExitStatus(new ExitStatus("CONTINUABLE"));
-
-            return RepeatStatus.FINISHED;
-        };
+    public Tasklet guLawdCdTasklet(LawdRepository lawdRepository) {
+        return new GuLawdTasklet(lawdRepository);
     }
 
     @JobScope
